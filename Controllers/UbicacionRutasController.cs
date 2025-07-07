a0086b0fc44efa2a4864098b8deeb5f44fa80627
Pblackberry <tomaslopezandrade2004@gmail.com>
2025-07-06 22:12:54 -0500

    primer comit

---
+++ Controllers/UbicacionRutasController.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using ApiAppTransporte.Models;
+namespace ApiAppTransporte.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UbicacionRutasController : Controller
+    {
+        string connectionString = "Server=LAPTOP-OJJP1HNK;Database=SLR_DB;User Id=adminRutas;Password=admin;TrustServerCertificate=True;";
+        [Route("[action]")]
+        [HttpGet]
+        public ActionResult GetUbicacionActual(int rutaId)
+        {
+
+            string query = $"SELECT TOP 1 longitud, latitud FROM Ruta WHERE id = @rutaId";
+            Rutas.Coordenadas ubicacion = new();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@rutaId", rutaId);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            ubicacion.latitud = double.Parse(reader.GetValue(0).ToString());
+                            ubicacion.longitud = double.Parse(reader.GetValue(1).ToString());
+
+                        }
+                    }
+                }
+                
+            }
+            return Ok(ubicacion);
+
+        }
+    }
+}
---
+++ Models/Rutas.cs
@@ -0,0 +1,32 @@
+﻿namespace ApiAppTransporte.Models
+{
+    public class Rutas
+    {
+        public int? id;
+        public string? nombre;
+        public string? descripcion;
+        public int? conductorId;
+        public Coordenadas? coordenadas;
+        public class Coordenadas
+        {
+            public double? latitud { get; set; }
+            public double? longitud { get; set; }
+
+            public Coordenadas()
+            {
+            }
+        }
+
+        public Rutas()
+        {
+        }
+
+        public Rutas(int? id, string? nombre, string? descripcion, int? conductorId)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.conductorId = conductorId;
+        }
+    }
+}
